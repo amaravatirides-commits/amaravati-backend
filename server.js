343bde1691903a66635198624db937650fa57cc3
M S K Reddy <your-email@example.com>
2025-08-22 15:41:21 +0530

    Update .gitignore and server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const connectDB = require("./config/db");
 // Connect to MongoDB first, then start server
 connectDB()
   .then(() => {
-    const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT || 5000; // Render will provide its own port
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
